759e9e9151df7329e00a1bdea27601db6337913d
Juanma Coria <juanmc2005@hotmail.com>
2019-03-24 19:43:40 +0100

    Cleaned explore script

--- explore.jl
+++ explore.jl
@@ -1,12 +1,5 @@
 using WAV, DataFrames, CSV, PlotlyJS, DSP, PyPlot
-
-function gen_label_dict(df_labels)
-    labels_dict = Dict()
-    for i in 1:size(df_labels, 1)
-        labels_dict[df_labels[i,1]] = df_labels[i,2]
-    end
-    return labels_dict
-end
+include("spectrograms.jl")
 
 function plot_classes(classes, frequencies)
     plot(bar(;x=classes, y=frequencies))
@@ -30,20 +23,9 @@ function create_audio_metadata(labels)
     return audiodata
 end
 
-function create_spectrogram(wavfile)
-    signal, sampleRate = wavread(wavfile)
-    spec = spectrogram(signal[:,1], convert(Int, round(25e-3*fs)),
-                        convert(Int, 10e-3*fs); window=hanning)
-    t = time(spec)
-    f = freq(spec)
-    return log10.(power(spec))
-end
-
 # Read class data
 df = CSV.read("train/train.csv", header=true)
-# Read label dictionary : (sound id) => class
-labels = gen_label_dict(df)
-delete!(labels, 8542) # Bad file
+labels = gen_label_dict("train/train.csv")
 
 # Plot class distribution
 classdata = by(df, :Class, nrow)
